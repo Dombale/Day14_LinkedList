16a34794a77c878cd6dfca85b91ecb319677ec9d
Priyanka <priyankadombale27@gmail.com>
2022-03-03 22:32:34 +0530

    Added : Create Linklist

---
+++ src/com/blz/LinkedList1.java
@@ -0,0 +1,18 @@
+package com.blz;
+
+public class LinkedList1 {
+	Node head;
+
+	// node creation
+	public class Node {
+
+		Object data;
+		Node ref;
+
+		// constructor
+		public Node(Object data) {
+			this.data = data;
+		}
+	}
+
+}
